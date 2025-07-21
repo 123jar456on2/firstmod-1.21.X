1746537b9260fdc4cf8b6ef4d058dd8690db61bc
BuildTools <unconfigured@null.spigotmc.org>
2025-07-21 17:43:21 -0600

    added important comment

--- src/main/java/com/brickseverywhere/firstmod/FirstMod.java
+++ src/main/java/com/brickseverywhere/firstmod/FirstMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very important thingy
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
 
